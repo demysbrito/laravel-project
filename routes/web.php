03830c03d878e13f99de841dcbc628787e303b9c
demysbrito <demysbrito@gmail.com>
2024-06-28 14:00:03 -0300

    Adding contact and about pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// Route::get('/second', function () {
+//     return view('second');
+// });
+
+
+Route::view('/second', 'second'); 
+
+Route::view('/', 'home'); 
+Route::view('contact', 'contact');
+Route::view('about', 'about');
